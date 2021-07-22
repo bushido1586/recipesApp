922e0098572217ed4b2ac1a80e110ed2b9e19a46
Bradley Reed <shenmusha@yahoo.com>
2021-07-21 20:27:39 -0400

    work has begun

---
+++ shared/Pastas.js
@@ -0,0 +1,17 @@
+export const PASTAS = [
+    {
+        id: 0, 
+        name: "Cajun Chicken Pasta",
+        cookTime: 'Aprrox. 30 mins', 
+        featured: false, 
+        image: '',
+        ingredients: "", 
+        instructions: ""
+    }, 
+    {
+        id: 1, 
+        name: "Pesto Chicken Pasta",
+        cookTime: "approx 30 mins",
+
+    }
+];
