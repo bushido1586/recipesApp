e40b255503a25a5a2ac669a2bcfe53b9662604d1
Bradley Reed <shenmusha@yahoo.com>
2021-07-24 12:25:28 -0400

    missed a save

---
+++ components/HomeComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Home extends Component{
+
+    static navigationOptions = {
+        title: 'Home'
+    }
+    
+    render(){
+        return(
+            <ScrollView>
+
+            </ScrollView>
+        );
+    }
+}
+
+export default Home;
