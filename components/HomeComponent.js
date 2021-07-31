81342099afb240fec6ce601cfe16f7e77adb0f25
Bradley Reed <shenmusha@yahoo.com>
2021-07-31 12:23:54 -0400

    adding components to home

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 
+function RenderFavorites(props){
+    const {favorite}=props
+
+    if(favorite){
+        return(
+            <Card>
+                <Text>This will have an item to display</Text>
+            </Card>
+        )
+    }
+    return <View />
+}
+
 
 class Home extends Component{
    
@@ -13,7 +26,7 @@ class Home extends Component{
         return(
             <View>
                 <Text>Welcome to my new Recipe App! Here are my favorites!Check out the menu for more!</Text>
-                
+                <RenderFavorites />
             </View>
         );
     }
