4077af7a68bb454b8d08bf037e1ba76ae79dc111
Bradley Reed <shenmusha@yahoo.com>
2021-07-31 11:55:59 -0400

    added drink file

---
+++ shared/Drinks.js
@@ -0,0 +1,12 @@
+export const SAUCES = [
+    {
+        id: 0, 
+        name: "Rebellious Fish",
+        cookTime: '15 mins of prep', 
+        favorite: true, 
+        image: '',
+        ingredients: "1.5 oz Russian Standard Vodka", 
+        instructions: "",
+        description:'A sweet and fruity cocktail best enjoyed by the pool!'
+    }
+]
