932822c743528bae71853f0ec29fae6ea47e240e
Bradley Reed <shenmusha@yahoo.com>
2021-08-12 00:43:40 -0400

    drinks render

--- components/DrinksComponent.js
+++ components/DrinksComponent.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { Card } from 'react-native-elements';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+
 
 export const DRINKS = [
     {
-        id: 0, 
+        drinksId: 0, 
         title: "Rebellious Fish",
         prep: '15 mins of prep', 
-        featured: true, 
+        favorite: false, 
         image: '',
-        ingredients: "1.5 oz Russian Standard Vodka", 
-        instructions: "",
+        ingredients: "1.5 oz Russian Standard Vodka \n1.5 oz Passoa passion fruit Liquor \n6oz Orange Juice \n~3oz prosecco", 
+        instructions: "In a shaker add vodka, Passoa, and orange juice over ice \n shake until mixed \n Strain into glass over ice \n top with prosecco.",
         description:'A sweet and fruity cocktail best enjoyed by the pool!'
     }
 ];
 
-const Item =({title}) =>(
+const TitleItem =({title}) =>(
     <View>
-        <Card>
-            <Text>{title}</Text>
-        </Card>
+        <Text>{title}</Text>
+    </View>
+)
+const PrepItem=({prep}) =>(
+    <View>
+        <Text>{prep}</Text>
+    </View>
+)
+const IngredientsItem =({ingredients}) =>(
+    <View>
+        <Text>{ingredients}</Text>
+    </View>
+)
+const InstructionsItem =({instructions}) =>(
+    <View>
+        <Text>{instructions}</Text>
     </View>
 )
 
+
 const renderItem = ({item})=>(
-    <Item title={item.title} />
+    <Card>
+        <TitleItem title={item.title} />
+        <PrepItem prep={item.prep}/>
+        <IngredientsItem ingredients={item.ingredients} />
+        <InstructionsItem instructions={item.instructions} />
+        <Icon 
+            name='heart-o'
+            type='font-awesome'
+            raised
+        />
+    </Card>
 );
 
 class Drinks extends Component{
@@ -32,19 +57,10 @@ class Drinks extends Component{
     static navigationOptions = {
         title: 'Drinks'
     }
-    
-     Item =({title}) =>(
-        <View>
-            <Card>
-                <Text>{title}</Text>
-            </Card>
-        </View>
-    )
-    
-    renderItem = ({item})=>(
-        <Item title={item.title} />
-    );
 
+    markFavorite(drinksId) {
+        this.props.postFavorite(drinksId);
+    }
 
     render(){
         return(
@@ -52,7 +68,7 @@ class Drinks extends Component{
             <FlatList 
                 data={DRINKS}
                 renderItem={renderItem}
-                keyExtractor={item => item.id}
+                keyExtractor={item => item.drinksId}
             />
             <Text>This portion is under construction, check back soon for more fun recipes!</Text>
         </View>
@@ -60,4 +76,14 @@ class Drinks extends Component{
     }
 }
 
+const styles =StyleSheet.create({
+    container:{
+        flex:1
+    },
+    title :{
+
+    }
+})
+
 export default Drinks;
+
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -1,1 +1,2 @@
 import * as ActionTypes from './ActionTypes';
+
